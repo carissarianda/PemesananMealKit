932a0437e264fcc4bd53466775202cb0382f147e
carissarianda <16521473@mahasiswa.itb.ac.id>
2023-11-30 15:38:55 +0700

    tamat

--- TST/pemesanan.py
+++ TST/pemesanan.py
@@ -8,31 +8,20 @@ from passlib.context import CryptContext
 from pydantic import BaseModel
 import json
 
-SECRET_KEY = "d0ba1b2795d465f636c24af3980de67546109235434a71c90f474b21ea61899f7"
+SECRET_KEY = "25ce8b918a5d21abec4a39f7ba25c245699f245ac8bdcb73ead119543a6d1e0b"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 6000
 
 class InputUser(BaseModel):
-    id_user : int
-    nama_user : str
-    id_barang : int
-    nama_barang : str
-    jumlah : int
-    waktu : datetime
+    username : str
+    nama_user: str
+    id_barang: int
+    jumlah: int
     
 class DataUser(BaseModel):
     username : str
     nama_user: str
-    disabled: bool or None = None
-    
-db = {
-    "carissa": {
-        "username": "carissa",
-        "nama_user": "carissa",
-        "hashed_password": "$2b$12$eYicYMBGBlvM/jGJe9VXP.k7ujIhD8fSVkgi1T.nyOb0Xrq/ienia",
-        "disabled": False,
-    }
-}
+    password: str
  
 class Token(BaseModel):
     access_token : str
@@ -42,7 +31,11 @@ class TokenData(BaseModel):
     username: str or None = None
     
 class UserInDB(DataUser):
+    id_user : int
+    username : str
     hashed_password: str
+    disabled: bool or None = None
+
     
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -67,18 +60,31 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
+def get_user(username: str):
+    for user in user_pemesanan['user_pemesanan']:
+        if user['username'] == username:
+           user_dict = {
+               "id_user" : user['id_user'],
+                "username" :  user['username'],
+                "nama_user": user['nama_user'],
+               "hashed_password": user['password'],
+               "password" : ""
+           }
+           return UserInDB(**user_dict)
+    return None
 
-def authenticate_user(db, username: str, password: str):
-    user = get_user(db, username)
+
+
+
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
         return False
     return user
+
+
         
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
@@ -104,7 +110,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(db, username=token_data.username)
+    user = get_user(username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
@@ -116,12 +122,35 @@ async def get_current_active_user(
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
+@app.post("/register", tags=['Register'])
+async def register_user(data: DataUser):
+    dataUser_found = False
 
-@app.post("/token", response_model=Token)
+    for user in user_pemesanan['user_pemesanan']:
+        if user['nama_user'] == data.username:
+            dataUser_found = True
+            return f"User {user['id_user']} dengan username {user['nama_user']} telah terdaftar."
+
+    if not dataUser_found:
+        i = len(user_pemesanan['user_pemesanan']) + 1
+        result = {
+            "id_user": i,
+            "nama_user": data.nama_user,
+            "username" : data.username,
+            "password": get_password_hash(data.password)
+        }
+        user_pemesanan['user_pemesanan'].append(result)
+        save_result_to_json(user_pemesanan, "user_pemesanan.json")
+        return {"message": "User berhasil mendaftar"}
+
+    raise HTTPException(
+        status_code=404, detail=f'User not found'
+    )
+@app.post("/token", response_model=Token, tags=['Generate Token'])
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 ):
-    user = authenticate_user(db, form_data.username, form_data.password)
+    user = authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -134,7 +163,6 @@ async def login_for_access_token(
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
-
 @app.get("/users/me/", response_model=DataUser)
 async def read_users_me(
     current_user: Annotated[DataUser, Depends(get_current_active_user)]
@@ -142,132 +170,130 @@ async def read_users_me(
     return current_user
 
 
-@app.get("/users/me/items/")
-async def read_own_items(
-    current_user: Annotated[DataUser, Depends(get_current_active_user)]
+@app.put("/update/me", tags=['Auth Users'])
+async def update_my_data(
+    current_user: Annotated[DataUser, Depends(get_current_active_user)],
+    data: DataUser
 ):
-    return [{"item_id": "Foo", "owner": current_user.username}]
+    # Check if the user making the request is the same as the one being updated
+    if current_user.username != data.username:
+        raise HTTPException(
+            status_code=403,
+            detail="You can only update your own data",
+        )
 
-# Mendapatkan seluruh riwayat hasil pemesanan
-@app.get('/hasil')
-async def read_data_hasil_pemesanan():
-	return hasil_pemesanan['hasil_pemesanan']
+    # Update the user data here
+    current_user.username = data.username
+    current_user.nama_user = data.nama_user
+    current_user.password = get_password_hash(data.password)
+
+    # Save the updated user data back to your storage (e.g., JSON file)
+    save_result_to_json(user_pemesanan, "user_pemesanan.json")
+
+    return {"message": "User data updated successfully"}
+
 
 # Mendapatkan seluruh data barang
 @app.get('/barang')
 async def read_data_barang():
 	return data_barang['data_barang']
 
-# Mendapatkan riwayat hasil pemesanan untuk user_id tertentu
-@app.get('/hasil/{user_id}')
-async def get_data_hasil_pemesanan_user(user_id: int):
+@app.get('/hasil', response_model=list[InputUser], tags=['Get User Pemesanan'])
+async def get_data_hasil_pemesanan_user(
+    current_user: Annotated[DataUser, Depends(get_current_active_user)]
+):
+    username: str = current_user.username
+
     matching_user = []
     for hasil in hasil_pemesanan['hasil_pemesanan']:
-        if hasil['id_user'] == user_id:
-            matching_user.append(hasil_pemesanan)
+        if 'username' in hasil and hasil['username'] == username:
+            matching_user.append(hasil)
     
-    if not matching_user :
+    if not matching_user:
         raise HTTPException(
-		    status_code=404, detail=f'User tidak terdaftar'
-	)
+            status_code=404, detail='User tidak memiliki data hasil pemesanan'
+        )
 
     return matching_user
 
-# Melakukan post ke dalam JSON user_pemesanan dan mengembalikan daftar user yang memesan
-@app.post('/add_user')
-async def add_user(dataUser : DataUser):
-	data = dataUser.dict()
-	dataUser_found = False
-	for user in user_pemesanan['user_pemesanan']:
-		if user['id_user'] == data['id_user'] or user['nama_user'] == data['nama_user']:
-			dataUser_found = True
-			return "User " + str(user['id_user']) + " dengan username " + str(user['nama_user']) + " telah terdaftar."
-	
-	if not dataUser_found:
-		user_pemesanan['user_pemesanan'].append(data)
-		save_result_to_json(user_pemesanan, "user_pemesanan.json")
-		return data
-	raise HTTPException(
-		status_code=404, detail=f'user not found'
-	)
+
 
 #Melakukan post ke JSON hasil_pemesanan 
-@app.post('/pemesanan')
-async def add_hasil_pemesanan(item: InputUser):
-    data = item.dict()
+@app.post('/pemesanan', response_model=dict, tags=['Add Hasil Pemesanan'])
+async def add_hasil_pemesanan(
+    current_user: Annotated[DataUser, Depends(get_current_active_user)],
+    data: InputUser
+):
+    # Check if the username in the submitted data matches the username of the current user
+    if data.username != current_user.username:
+        raise HTTPException(
+            status_code=403,
+            detail="You can only add hasil pemesanan for yourself",
+        )
+
     i = 1
-    jumlah = data.get("jumlah")
-    data['waktu'] = data['waktu'].isoformat()
+    jumlah = data.jumlah
+    data_dict = data.dict()
     barang_found = False
     stok_tersedia = False
-    user_found = False
-    id_barang = data.get("id_barang")
+    nominal = 0
+
     for barang in data_barang['data_barang']:
-        if barang['id_barang'] == data['id_barang']:
+        if barang['id_barang'] == data_dict['id_barang']:
             barang_found = True
             if barang['stok'] >= jumlah:
                 stok_tersedia = True
                 hasilPemesanan = "Anda berhasil memesan barang"
                 nominal = jumlah * barang['harga']
                 stok = barang['stok'] - jumlah
-            else :
+            else:
                 hasilPemesanan = "Anda tidak berhasil memesan barang"
             break
-
-    if not barang_found :
-        return "Barang tidak tersedia"
-    if not stok_tersedia :
-        return "Anda tidak berhasil memesan barang"
-
-
-# Update stok barang di dalam data_barang.json
-    for user in user_pemesanan['user_pemesanan']:
-        if user['id_user'] == data['id_user']:
-            user_found = True
+    if not stok_tersedia:
+        hasilPemesanan = "Anda tidak berhasil memesan barang, Stok habis"
+    if not barang_found:
+        hasilPemesanan = "Barang tidak tersedia"
+    
     for barang in data_barang['data_barang']:
-        if barang['id_barang'] == data['id_barang'] and user_found == True:
+        if barang['id_barang'] == data_dict['id_barang']:
                 barang['stok'] -= jumlah
-
-# Simpan perubahan stok ke dalam file data_barang.json
+        
     with open("data_barang.json", "w") as write_file:
-        json.dump(data_barang, write_file)
-	
+                    json.dump(data_barang, write_file)
+
+    # Add hasil pemesanan for the current user
     for user in user_pemesanan['user_pemesanan']:
-        if user['id_user'] == data['id_user']:
-            for count in hasil_pemesanan['hasil_pemesanan']:
-                i += 1
+        for count in hasil_pemesanan['hasil_pemesanan']:
+            i += 1
             result = {
                 "id_pesanan": i,
-                "id_user" : data['id_user'],
-                "nama_user": data['nama_user'],
-                "id_barang": data['id_barang'],
-                "nama_barang": data['nama_barang'],
+                "username" : data.username,
+                "nama_user": data.nama_user,
+                "id_barang": data.id_barang,
                 "jumlah" : jumlah,
                 "nominal" : nominal,
                 "hasilPemesanan" : hasilPemesanan,
-                "waktu" : data['waktu']
             }
             hasil_pemesanan['hasil_pemesanan'].append(result)
             save_result_to_json(hasil_pemesanan, "hasil_pemesanan.json")
-            return hasilPemesanan
+            return {"message": hasilPemesanan}
     raise HTTPException(
 		status_code=404, detail=f'user not found'
 	)
 
-@app.delete('/user/{user_id}')
-async def delete_user(user_id: int):
-	user_found = False
-	for user_idx, user_item in enumerate(user_pemesanan['user_pemesanan']):
-		if user_item['id_user'] == user_id:
-			user_found = True
-			user_pemesanan['user_pemesanan'].pop(user_idx)
-			
-			save_result_to_json(user_pemesanan, "user_pemesanan.json")
-			return "updated"
-	
-	if not user_found:
-		return "User ID not found."
-	raise HTTPException(
-		status_code=404, detail=f'User not found'
-	)
+@app.delete('/user/me', tags=['Delete User'])
+async def delete_authenticated_user(current_user: Annotated[DataUser, Depends(get_current_active_user)]):
+    user_id = current_user.id_user
 
+    user_found = False
+    for user_idx, user_item in enumerate(user_pemesanan['user_pemesanan']):
+        if user_item['id_user'] == user_id:
+            user_found = True
+            user_pemesanan['user_pemesanan'].pop(user_idx)
+            save_result_to_json(user_pemesanan, "user_pemesanan.json")
+            return {"message": "User deleted successfully"}
+
+    if not user_found:
+        raise HTTPException(
+            status_code=404, detail='User not found'
+        )
